603641cfa892c365fc3b8dcbc59bde6873da3995
Dhruba1010 <dhrubasinha096p@gmail.com>
2026-09-15 21:49:45 +0530

    feat(application): add historical market data repository contract

--- src/northstar_application/ports/__init__.py
+++ src/northstar_application/ports/__init__.py
@@ -3,6 +3,11 @@
 from northstar_application.ports.broker_port import BrokerPort
 from northstar_application.ports.event_publisher_port import EventPublisherPort
 from northstar_application.ports.exchange_port import ExchangePort
+from northstar_application.ports.historical_market_data_repository import (
+    HistoricalMarketDataQuery,
+    HistoricalMarketDataRepository,
+    InvalidHistoricalMarketDataQueryError,
+)
 from northstar_application.ports.market_observation_source import MarketObservationSource
 from northstar_application.ports.order_persistence_port import OrderPersistencePort
 from northstar_application.ports.portfolio_update_port import PortfolioUpdatePort
@@ -13,6 +18,9 @@ __all__ = [
     "BrokerPort",
     "EventPublisherPort",
     "ExchangePort",
+    "HistoricalMarketDataQuery",
+    "HistoricalMarketDataRepository",
+    "InvalidHistoricalMarketDataQueryError",
     "MarketObservationSource",
     "OrderPersistencePort",
     "PortfolioUpdatePort",
